dc08b2f718581365da56c41041fb5e1bfbbc86fd
muyongKim <wykim970@gmail.com>
2022-10-19 16:56:45 +0900

    Initial commit

---
+++ 1Week_Mission/src/test/java/com/lion/ebookmarket/EBookMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lion.ebookmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBookMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
